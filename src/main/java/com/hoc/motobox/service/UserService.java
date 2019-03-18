a8908b6d11aba360dbd53a922eec3f3e253c9965
tareq-salem <38789773+tareq-salem@users.noreply.github.com>
2019-03-18 15:44:20 +0100

    modification de routes pour l'ajout d'ad ,affichage d'ad....;

--- src/main/java/com/hoc/motobox/service/UserService.java
+++ src/main/java/com/hoc/motobox/service/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.hoc.motobox.entity.Ad;
 import com.hoc.motobox.entity.User;
 import com.hoc.motobox.repository.RoleRepository;
 import com.hoc.motobox.repository.UserRepository;
@@ -17,48 +18,50 @@ import com.hoc.motobox.utils.SuperRestService;
 @Service
 public class UserService extends InitialDataLoader implements SuperRestService<User> {
 
-    private static final Logger LOGGER = LogManager.getLogger(User.class);
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+	private static final Logger LOGGER = LogManager.getLogger(User.class);
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private RoleRepository roleReposytory;
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private RoleRepository roleReposytory;
 
-    @Override
-    public JpaRepository<User, Long> getDao() {
-        return userRepository;
-    }
+	@Override
+	public JpaRepository<User, Long> getDao() {
+		return userRepository;
+	}
 
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userRepository = userRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
+	public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.userRepository = userRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
 
-    @Override
-    public User save(User user) throws EntityExistsException {
-        LOGGER.debug("create user {}", user);
+	@Override
+	public User save(User user) throws EntityExistsException {
+		LOGGER.debug("create user {}", user);
 
-        if (emailExists(user.getEmail())) {
-            throw new EntityExistsException("There is an account with that email adress: " + user.getEmail());
-        }
-        User createUser = new User();
+		if (emailExists(user.getEmail())) {
+			throw new EntityExistsException("There is an account with that email adress: " + user.getEmail());
+		}
+		User createUser = new User();
 
-        createUser.setEmail(user.getEmail());
-        createUser.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        createUser.setFirstName(user.getFirstName());
-        createUser.setLastName(user.getLastName());
-        createUser.setPhone(user.getPhone());
-        createUser.setAddress(user.getAddress());
-        if (user.getRole() != null) {
-            createUser.setRole(roleReposytory.findByName("JUNKYARD"));
-        }
+		createUser.setEmail(user.getEmail());
+		createUser.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		createUser.setFirstName(user.getFirstName());
+		createUser.setLastName(user.getLastName());
+		createUser.setPhone(user.getPhone());
+		createUser.setAddress(user.getAddress());
+		if (user.getRole() != null) {
+			createUser.setRole(roleReposytory.findByName(user.getRole().getName()));
+		}
 
-        return userRepository.save(createUser);
+		return userRepository.save(createUser);
 
-    }
+	}
 
-    private boolean emailExists(final String email) {
-        return userRepository.findByEmail(email) != null;
-    }
+	private boolean emailExists(final String email) {
+		return userRepository.findByEmail(email) != null;
+	}
+	
+	
 }
